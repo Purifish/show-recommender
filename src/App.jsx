a77b1990ae767f8761cbff1672d02f8be6a6340a
Muqaffa <purifish97@gmail.com>
2023-10-16 10:24:31 +0800

    (1) minor bugfix (2) add randomized main TV show

--- src/App.jsx
+++ src/App.jsx
@@ -11,91 +11,100 @@ import SearchBar from "./components/SearchBar/SearchBar";
 import logoImg from "./assets/images/logo.png";
 
 function App() {
+  const [curTvShow, setCurTvShow] = useState();
+  const [recommendationList, setRecommendationList] = useState([]);
 
-    const [curTvShow, setCurTvShow] = useState();
-    const [recommendationList, setRecommendationList] = useState([]);
+  async function fetchPopulars() {
+    try {
+      const popularShowsData = await TvShowApi.fetchPopulars();
+      if (popularShowsData.length > 0) {
+        const upperBound = Math.min(popularShowsData.length, 10);
+        const selectedIndex = Math.trunc(Math.random() * upperBound);
 
-    async function fetchPopulars() {
-        try {
-            const popularShowsData = await TvShowApi.fetchPopulars();
-            if (popularShowsData.length > 0) {
-                setCurTvShow(popularShowsData[0]); // take the currently most popular show
-            }
-        } catch (err) {
-            console.log("Error fetching popular tv show.");
-        }
+        setCurTvShow(popularShowsData[selectedIndex]); // take the currently most popular show
+      }
+    } catch (err) {
+      console.log("Error fetching popular tv show.");
     }
+  }
 
-    async function fetchRecommendations(tvShowId) {
-        try {
-            const recommendedShowsData = await TvShowApi.fetchRecommendations(tvShowId);
-            if (recommendedShowsData.length > 0) {
-                setRecommendationList(recommendedShowsData.slice(0, 10)); // take the first 10 recommended tv shows
-            }
-        } catch (err) {
-            console.log("Error fetching recommendations.");
-        }
-        
+  async function fetchRecommendations(tvShowId) {
+    try {
+      const recommendedShowsData = await TvShowApi.fetchRecommendations(
+        tvShowId
+      );
+      if (recommendedShowsData.length > 0) {
+        setRecommendationList(recommendedShowsData.slice(0, 10)); // take the first 10 recommended tv shows
+      }
+    } catch (err) {
+      console.log("Error fetching recommendations.");
     }
+  }
 
-    async function fetchByTitle(title) {
-        try {
-            const searchResult = await TvShowApi.fetchByTitle(title);
-            if (searchResult.length > 0) {
-                setCurTvShow(searchResult[0]);
-            } else {
-                console.log("Problem detected when fetching search result");
-            }
-        } catch (err) {
-            console.log("Error fetching search result");
-        }
+  async function fetchByTitle(title) {
+    try {
+      const searchResult = await TvShowApi.fetchByTitle(title);
+      if (searchResult.length > 0) {
+        setCurTvShow(searchResult[0]);
+      } else {
+        console.log("Problem detected when fetching search result");
+      }
+    } catch (err) {
+      console.log("Error fetching search result");
     }
+  }
 
-    function updateCurrentShow(tvShow) {
-        setCurTvShow(tvShow);
+  function updateCurrentShow(tvShow) {
+    setCurTvShow(tvShow);
+  }
+
+  useEffect(() => {
+    fetchPopulars();
+  }, []);
+
+  useEffect(() => {
+    if (curTvShow) {
+      fetchRecommendations(curTvShow.id);
     }
+  }, [curTvShow]);
 
-    useEffect(() => {
-        fetchPopulars();
-    }, []);
-
-    useEffect(() => {
-        if (curTvShow) {
-            fetchRecommendations(curTvShow.id);
-        }
-    }, [curTvShow]);
-
-    return (
-        <div 
-            className={s.main_container}
-            style={{
-                background: curTvShow ? `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url("${BACKDROP_BASE_URL}${curTvShow.backdrop_path}") no-repeat center / cover`
-                : "black"
-            }} // background
-        
-        >
-            <div className={s.header}>
-                <div className="row"> 
-                    {/*Bootstrap rows and columns*/}
-                    <div className="col-4">
-                        <Logo img={logoImg} title="BingeBuddy" subtitle="Find a show suited for you!"/>
-                    </div>
-                    <div className="col-md-12 col-lg-4">
-                        <SearchBar onSubmit={fetchByTitle}/>
-                    </div>
-                
-                </div>
-            </div>
-            <div className={s.tv_show_detail}>
-                {curTvShow && <TvShowDetail tvShow={curTvShow}/>}
-            </div>
-            <div className={s.recommended_tv_shows}>
-                {recommendationList && <TvShowList onClickItem={updateCurrentShow} tvShows={recommendationList} />}
-            </div>
-
+  return (
+    <div
+      className={s.main_container}
+      style={{
+        background: curTvShow
+          ? `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url("${BACKDROP_BASE_URL}${curTvShow.backdrop_path}") no-repeat center / cover`
+          : "black",
+      }} // background
+    >
+      <div className={s.header}>
+        <div className="row">
+          {/*Bootstrap rows and columns*/}
+          <div className="col-4">
+            <Logo
+              img={logoImg}
+              title="BingeBuddy"
+              subtitle="Find a show suited for you!"
+            />
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <SearchBar onSubmit={fetchByTitle} />
+          </div>
         </div>
-
-    );
+      </div>
+      <div className={s.tv_show_detail}>
+        {curTvShow && <TvShowDetail tvShow={curTvShow} />}
+      </div>
+      <div className={s.recommended_tv_shows}>
+        {recommendationList && (
+          <TvShowList
+            onClickItem={updateCurrentShow}
+            tvShows={recommendationList}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/TvShowListItem/TvShowListItem.jsx
+++ src/components/TvShowListItem/TvShowListItem.jsx
@@ -4,22 +4,30 @@ import { SMALL_IMAGE_BASE_URL } from "../../config";
 const MAX_TITLE_LENGTH = 20;
 
 function TvShowListItem(props) {
-    const { tvShow, onClick } = props;
+  const { tvShow, onClick } = props;
 
-    function onClick_() {
-        onClick(tvShow);
-    }
+  function onClick_() {
+    onClick(tvShow);
+  }
 
-    return (
-        <div onClick={onClick_} className={s.container}>
-            <img 
-                alt={tvShow.name} 
-                src={SMALL_IMAGE_BASE_URL + tvShow.backdrop_path}
-                className={s.img}
-            />
-            <div className={s.title}>{tvShow.name.length <= 20 ? tvShow.name : tvShow.name.slice(0, MAX_TITLE_LENGTH) + "..."}</div>
-        </div>
-    )
+  if (!tvShow.backdrop_path) {
+    return <></>;
+  }
+
+  return (
+    <div onClick={onClick_} className={s.container}>
+      <img
+        alt={tvShow.name}
+        src={SMALL_IMAGE_BASE_URL + tvShow.backdrop_path}
+        className={s.img}
+      />
+      <div className={s.title}>
+        {tvShow.name.length <= 20
+          ? tvShow.name
+          : tvShow.name.slice(0, MAX_TITLE_LENGTH) + "..."}
+      </div>
+    </div>
+  );
 }
 
 export default TvShowListItem;
